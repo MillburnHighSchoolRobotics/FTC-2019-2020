e5041fe4124517441044db5ea212abe9eda71677
unknown <aemb011@gmail.com>
2019-05-17 16:59:19 -0400

    movement encoder stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Movement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Movement.java
@@ -9,55 +9,55 @@ import org.firstinspires.ftc.robotcore.internal.opmode.OpModeManagerImpl;
 import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
 
 public class Movement {
-    DcMotorEx lf;
-    DcMotorEx lb;
-    DcMotorEx rf;
-    DcMotorEx rb;
-    double ticksPerRev = 730;
-    double wheelRadius = 2;
-    public Movement(DcMotor lf, DcMotor lb, DcMotor rf, DcMotor rb) {
-        this.lf = (DcMotorEx)lf;
-        this.lb = (DcMotorEx)lb;
-        this.rf = (DcMotorEx)rf;
-        this.rb = (DcMotorEx)rb;
-    }
-    public void translateDistance(double power, double distance) throws InterruptedException {
-        int encoders = (int)(distance/(2*Math.PI*wheelRadius) * ticksPerRev);
-        lf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        lb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        moveToPosition(new DcMotor[] {lf,lb,rf,rb}, new double[] {-power,-power,power,power}, new int[] {(encoders),(encoders),(encoders),(encoders)});
-    }
-    public void moveToPosition(DcMotor[] motors, double[] power, int[] position) throws InterruptedException {
-        for (int x = 0; x < motors.length; x++) {
-            motors[x].setPower(power[x]);
-            motors[x].setTargetPosition(position[x]);
-            motors[x].setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        }
-
-        while (!shouldStop()) {
-            if (Thread.currentThread().isInterrupted()) {
-                throw new InterruptedException();
-            }
-            for (int x = 0; x < motors.length; x++) {
-                if (!motors[x].isBusy() || (Math.abs(motors[x].getCurrentPosition()-position[x]) < 50)) {
-                    break;
-                }
-            }
-            Thread.sleep(10);
-        }
-        for (DcMotor motor : motors) {
-            motor.setPower(0);
-            motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        }
-    }
-    public static boolean shouldStop() {
-        Activity currActivity = AppUtil.getInstance().getActivity();
-        OpModeManagerImpl manager = OpModeManagerImpl.getOpModeManagerOfActivity(currActivity);
-        OpMode currentOpMode = manager.getActiveOpMode();
-        return currentOpMode instanceof LinearOpMode &&
-                ((LinearOpMode) currentOpMode).isStarted() &&
-                ((LinearOpMode) currentOpMode).isStopRequested();
-    }
+//    DcMotorEx lf;
+//    DcMotorEx lb;
+//    DcMotorEx rf;
+//    DcMotorEx rb;
+//    double ticksPerRev = 730;
+//    double wheelRadius = 2;
+//    public Movement(DcMotor lf, DcMotor lb, DcMotor rf, DcMotor rb) {
+//        this.lf = (DcMotorEx)lf;
+//        this.lb = (DcMotorEx)lb;
+//        this.rf = (DcMotorEx)rf;
+//        this.rb = (DcMotorEx)rb;
+//    }
+//    public void translateDistance(double power, double distance) throws InterruptedException {
+//        int encoders = (int)(distance/(2*Math.PI*wheelRadius) * ticksPerRev);
+//        lf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//        lb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//        rf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//        rb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//        moveToPosition(new DcMotor[] {lf,lb,rf,rb}, new double[] {-power,-power,power,power}, new int[] {(encoders),(encoders),(encoders),(encoders)});
+//    }
+//    public void moveToPosition(DcMotor[] motors, double[] power, int[] position) throws InterruptedException {
+//        for (int x = 0; x < motors.length; x++) {
+//            motors[x].setPower(power[x]);
+//            motors[x].setTargetPosition(position[x]);
+//            motors[x].setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//        }
+//
+//        while (!shouldStop()) {
+//            if (Thread.currentThread().isInterrupted()) {
+//                throw new InterruptedException();
+//            }
+//            for (int x = 0; x < motors.length; x++) {
+//                if (!motors[x].isBusy() || (Math.abs(motors[x].getCurrentPosition()-position[x]) < 50)) {
+//                    break;
+//                }
+//            }
+//            Thread.sleep(10);
+//        }
+//        for (DcMotor motor : motors) {
+//            motor.setPower(0);
+//            motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        }
+//    }
+//    public static boolean shouldStop() {
+//        Activity currActivity = AppUtil.getInstance().getActivity();
+//        OpModeManagerImpl manager = OpModeManagerImpl.getOpModeManagerOfActivity(currActivity);
+//        OpMode currentOpMode = manager.getActiveOpMode();
+//        return currentOpMode instanceof LinearOpMode &&
+//                ((LinearOpMode) currentOpMode).isStarted() &&
+//                ((LinearOpMode) currentOpMode).isStopRequested();
+//    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/MovementTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/MovementTest.java
@@ -10,25 +10,56 @@ import org.firstinspires.ftc.teamcode.Movement;
 @Autonomous(name = "Movement Test", group = "test")
 public class MovementTest extends LinearOpMode {
 
-    DcMotor lf;
-    DcMotor lb;
-    DcMotor rf;
-    DcMotor rb;
-
+    DcMotor motor;
+    DcMotor encoder;
+//    DcMotor lf;
+//    DcMotor lb;
+//    DcMotor rf;
+//    DcMotor rb;
+//
     @Override
     public void runOpMode() throws InterruptedException {
-        lf = hardwareMap.dcMotor.get("leftFront");
-        lb = hardwareMap.dcMotor.get("leftBack");
-        rf = hardwareMap.dcMotor.get("rightFront");
-        rb = hardwareMap.dcMotor.get("rightBack");
+        motor = hardwareMap.dcMotor.get("motor");
+        encoder = hardwareMap.dcMotor.get("motor");
 
-        rf.setDirection(DcMotorSimple.Direction.REVERSE);
-        rb.setDirection(DcMotorSimple.Direction.REVERSE);
-        lf.setDirection(DcMotorSimple.Direction.FORWARD);
-        lb.setDirection(DcMotorSimple.Direction.FORWARD);
+        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        encoder.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        Movement mv = new Movement(lf, lb, rf, rb);
+        motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        mv.translateDistance(0.7, 10);
+        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motor.setPower(730);
+        motor.setTargetPosition(1000);
+        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        while (!shouldStop()) {
+            if (Thread.currentThread().isInterrupted()) {
+                throw new InterruptedException();
+            }
+            for (int x = 0; x < motors.length; x++) {
+                if (!motors[x].isBusy() || (Math.abs(motors[x].getCurrentPosition()-position[x]) < 50)) {
+                    break;
+                }
+            }
+            Thread.sleep(10);
+        }
+        for (DcMotor motor : motors) {
+            motor.setPower(0);
+            motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
+    }
+//        lf = hardwareMap.dcMotor.get("leftFront");
+//        lb = hardwareMap.dcMotor.get("leftBack");
+//        rf = hardwareMap.dcMotor.get("rightFront");
+//        rb = hardwareMap.dcMotor.get("rightBack");
+//
+//        rf.setDirection(DcMotorSimple.Direction.REVERSE);
+//        rb.setDirection(DcMotorSimple.Direction.REVERSE);
+//        lf.setDirection(DcMotorSimple.Direction.FORWARD);
+//        lb.setDirection(DcMotorSimple.Direction.FORWARD);
+//
+//        Movement mv = new Movement(lf, lb, rf, rb);
+//
+//        mv.translateDistance(0.7, 10);
     }
 }
