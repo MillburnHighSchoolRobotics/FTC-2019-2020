bc1bc334bf68d83d6405cbef5aa464531ef170f1
SahilShah518 <sahilshah379@gmail.com>
2019-05-17 16:41:20 -0400

    imported stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/MovementTest.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode.Tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.teamcode.Movement;
+
+@Autonomous(name = "Movement Test", group = "test")
+public class MovementTest extends LinearOpMode {
+
+    DcMotor lf;
+    DcMotor lb;
+    DcMotor rf;
+    DcMotor rb;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        lf = hardwareMap.dcMotor.get("leftFront");
+        lb = hardwareMap.dcMotor.get("leftBack");
+        rf = hardwareMap.dcMotor.get("rightFront");
+        rb = hardwareMap.dcMotor.get("rightBack");
+
+        rf.setDirection(DcMotorSimple.Direction.REVERSE);
+        rb.setDirection(DcMotorSimple.Direction.REVERSE);
+        lf.setDirection(DcMotorSimple.Direction.FORWARD);
+        lb.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        Movement mv = new Movement(lf, lb, rf, rb);
+
+        mv.translateDistance(0.7, 10);
+    }
+}
