263359ad56063a21877be38e9ca15919497b4eb7
SahilShah518 <sahilshah379@gmail.com>
2019-06-05 15:50:24 -0400

    encoder stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/MovementTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/MovementTest.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.Tests;
 
 import android.app.Activity;
+import android.util.Log;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -13,46 +14,14 @@ import org.firstinspires.ftc.teamcode.Movement;
 @Autonomous(name = "Movement Test", group = "test")
 public class MovementTest extends LinearOpMode {
 
-    DcMotor lf;
-    DcMotor lb;
-    DcMotor rf;
-    DcMotor rb;
-    DcMotor ex1;
-    DcMotor ex2;
-    DcMotor ey;
+    DcMotor encoder;
 
     @Override
     public void runOpMode() throws InterruptedException {
-        lf = hardwareMap.dcMotor.get("lf");
-        lb = hardwareMap.dcMotor.get("lb");
-        rf = hardwareMap.dcMotor.get("rf");
-        rb = hardwareMap.dcMotor.get("rb");
-        ex1 = hardwareMap.dcMotor.get("ex1");
-        ex2 = hardwareMap.dcMotor.get("ex2");
-        ey = hardwareMap.dcMotor.get("ey");
-
-        lf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        lb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        rf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        rb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-        ex1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        ex2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        ey.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        ex1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        ex2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        ey.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        lf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        lb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        Movement mv = new Movement(lf, lb, rf, rb, ex1, ex2, ey);
-
+        encoder = hardwareMap.dcMotor.get("motor");
+        encoder.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        encoder.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         waitForStart();
-
-
+        Log.d("encoderPosition", String.valueOf(encoder.getCurrentPosition()));
     }
 }
