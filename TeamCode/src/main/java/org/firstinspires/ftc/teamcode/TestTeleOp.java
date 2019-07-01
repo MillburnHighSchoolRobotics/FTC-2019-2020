2308f619293f4e37563d3de33d0a752710e97b2c
mehulghosal <mehul.ghosal@gmail.com>
2019-07-01 14:20:43 -0400

    a

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOp.java
@@ -0,0 +1,122 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+
+import org.firstinspires.ftc.teamcode.RobotControl.MathUtils;
+
+import static com.qualcomm.robotcore.hardware.DcMotorSimple.Direction.REVERSE;
+
+@TeleOp(name = "TEST TELEOP", group = "Testing")
+public class TestTeleOp extends OpMode {
+    public DcMotorEx lf;
+    public DcMotorEx lb;
+    public DcMotorEx rf;
+    public DcMotorEx rb;
+    private double gearCoefficient = 1;
+    private double rotateCoefficient = 1;
+    public static final int[][] POWER_MATRIX = { //for each of the directions
+            {1, 1, 1, 1},
+            {1, 0, 0, 1},
+            {1, -1, -1, 1},
+            {0, -1, -1, 0},
+            {-1, -1, -1, -1},
+            {-1, 0, 0, -1},
+            {-1, 1, 1, -1},
+            {0, 1, 1, 0}
+    };
+    @Override
+    public void init() {
+        lf = (DcMotorEx)hardwareMap.dcMotor.get("lf");
+        lb = (DcMotorEx)hardwareMap.dcMotor.get("lb");
+        rf = (DcMotorEx)hardwareMap.dcMotor.get("rf");
+        rb = (DcMotorEx)hardwareMap.dcMotor.get("rb");
+        lf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        lb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        lf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        lb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        lf.setDirection(REVERSE);
+        lb.setDirection(REVERSE);
+    }
+
+    @Override
+    public void loop() {
+
+        if (gamepad1.x) {
+            lf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            lb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            rf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            rb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            lf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            lb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            rf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            rb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        }
+
+
+
+
+        double transX = gamepad1.left_stick_x;
+        double transY = -gamepad1.left_stick_y;
+        double rotX = gamepad1.right_stick_x;
+        double rotY = gamepad1.right_stick_y;
+        double translateMag = Math.sqrt(transX*transX + transY*transY);
+        double translateTheta = Math.atan2(transY, transX);
+        translateTheta = Math.toDegrees(translateTheta);
+        if (translateTheta < 0) translateTheta += 360;
+        double scale = 0;
+        double RF = 0, RB = 0, LF = 0, LB = 0;
+        if (!MathUtils.equals(rotX, 0, 0.05)) {
+            LF = rotX * rotateCoefficient;
+            LB = rotX * rotateCoefficient;
+            RF = -rotX * rotateCoefficient;
+            RB = -rotX * rotateCoefficient;
+        } else if (!MathUtils.equals(translateMag, 0, 0.05)) {
+            double translatePower = translateMag;
+            if (translateTheta >= 0 && translateTheta <= 90) { //quadrant 1
+                scale = MathUtils.sinDegrees(translateTheta - 45) / MathUtils.cosDegrees(translateTheta - 45);
+                LF = translatePower * POWER_MATRIX[0][0];
+                LB = translatePower * POWER_MATRIX[0][1] * scale;
+                RF = translatePower * POWER_MATRIX[0][2] * scale;
+                RB = translatePower * POWER_MATRIX[0][3];
+            } else if (translateTheta > 90 && translateTheta <= 180) { //quadrant 2
+                translatePower *= -1;
+                scale = MathUtils.sinDegrees(translateTheta - 135) / MathUtils.cosDegrees(translateTheta - 135);
+                LF = (translatePower * POWER_MATRIX[2][0] * scale);
+                LB = (translatePower * POWER_MATRIX[2][1]);
+                RF = (translatePower * POWER_MATRIX[2][2]);// * scale);
+                RB = (translatePower * POWER_MATRIX[2][3] * scale);
+            } else if (translateTheta > 180 && translateTheta <= 270) { //quadrant 3
+                scale = MathUtils.sinDegrees(translateTheta - 225) / MathUtils.cosDegrees(translateTheta - 225);
+                LF = (translatePower * POWER_MATRIX[4][0]);
+                LB = (translatePower * POWER_MATRIX[4][1] * scale);
+                RF = (translatePower * POWER_MATRIX[4][2] * scale);
+                RB = (translatePower * POWER_MATRIX[4][3]);
+            } else if (translateTheta > 270 && translateTheta < 360) { //quadrant 4
+                translatePower *= -1;
+                scale = MathUtils.sinDegrees(translateTheta - 315) / MathUtils.cosDegrees(translateTheta - 315);
+                LF = (translatePower * POWER_MATRIX[6][0] * scale);
+                LB = (translatePower * POWER_MATRIX[6][1]);
+                RF = (translatePower * POWER_MATRIX[6][2]);
+                RB = (translatePower * POWER_MATRIX[6][3] * scale);
+            }
+        }
+        lf.setPower(LF * gearCoefficient);
+        lb.setPower(LB * gearCoefficient);
+        rf.setPower(RF * gearCoefficient);
+        rb.setPower(RB * gearCoefficient);
+        telemetry.addData("THETA", translateTheta);
+        telemetry.addData("SCALE", scale);
+        telemetry.addData("LF", lf.getPower() + " " + lf.getCurrentPosition());
+        telemetry.addData("LB", lb.getPower() + " " +  lb.getCurrentPosition());
+        telemetry.addData("RF", rf.getPower() + " " + rf.getCurrentPosition());
+        telemetry.addData("RB", rb.getPower() + " " + rb.getCurrentPosition());
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/MotorIdentifier.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode.Tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name = "Motor Identifier", group = "Utilities")
+public class MotorIdentifier extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        DcMotor motor1 = hardwareMap.dcMotor.get("lf");
+        DcMotor motor2 = hardwareMap.dcMotor.get("lb");
+        DcMotor motor3 = hardwareMap.dcMotor.get("rf");
+        DcMotor motor4 = hardwareMap.dcMotor.get("rb");
+
+        motor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motor1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motor2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motor3.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motor3.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motor4.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motor4.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
+
+        Thread.sleep(1000);
+        motor1.setPower(1);
+        Thread.sleep(1000);
+        motor1.setPower(0);
+        motor2.setPower(1);
+        Thread.sleep(1000);
+        motor2.setPower(0);
+        motor3.setPower(1);
+        Thread.sleep(1000);
+        motor3.setPower(0);
+        motor4.setPower(1);
+        Thread.sleep(1000);
+        motor4.setPower(0);
+
+    }
+}
