5d7d0831e698a95a1e6bddb6d85ed85aafc337ee
SahilShah518 <sahilshah379@gmail.com>
2019-07-11 19:13:48 -0400

    to prove david that it works

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/FieldPositioningSystemTest.java
@@ -0,0 +1,57 @@
+package org.firstinspires.ftc.teamcode.Tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import org.firstinspires.ftc.teamcode.ThreadManager.PositionMonitor;
+import org.firstinspires.ftc.teamcode.ThreadManager.ThreadManager;
+
+@Autonomous(name = "This is to prove david that the fps works", group = "test")
+public class FieldPositioningSystemTest extends LinearOpMode {
+
+    DcMotorEx lf;
+    DcMotorEx lb;
+    DcMotorEx rf;
+    DcMotorEx rb;
+    DcMotorEx ex1;
+    DcMotorEx ex2;
+    DcMotorEx ey;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        lf = (DcMotorEx) hardwareMap.dcMotor.get("lf");
+        lb = (DcMotorEx) hardwareMap.dcMotor.get("lb");
+        rf = (DcMotorEx) hardwareMap.dcMotor.get("rf");
+        rb = (DcMotorEx) hardwareMap.dcMotor.get("rb");
+        ex1 = (DcMotorEx) hardwareMap.dcMotor.get("lf");
+        ex2 = (DcMotorEx) hardwareMap.dcMotor.get("lb");
+        ey = (DcMotorEx) hardwareMap.dcMotor.get("rf");
+        lf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        lb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        rf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        rb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        lf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        lb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        ex1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        ex1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        ex2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        ex2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        ey.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        ey.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        ThreadManager manager = ThreadManager.getInstance();
+        manager.setHardwareMap(hardwareMap);
+        manager.setupThread("PositionMonitor", PositionMonitor.class);
+        waitForStart();
+
+        while (true) {
+            telemetry.addData("theta", ThreadManager.getInstance().getValue("theta", Double.class));
+            telemetry.addData("x", ThreadManager.getInstance().getValue("x", Double.class));
+            telemetry.addData("y", ThreadManager.getInstance().getValue("y", Double.class));
+            telemetry.update();
+        }
+    }
+}
