a5ef7d22dcf389223c1a738a555677b00e82224c
unknown <aemb011@gmail.com>
2019-10-22 17:01:18 -0400

    LEGO(tm) dump truck 2: electric boogaloo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOp.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
 
 
 import org.firstinspires.ftc.teamcode.util.MathUtils;
@@ -12,10 +13,19 @@ import static com.qualcomm.robotcore.hardware.DcMotorSimple.Direction.REVERSE;
 
 @TeleOp(group = "test")
 public class TestTeleOp extends OpMode {
+    double[] squishPos = {0,1}; //open, close
+    double[] spinPos = {0,1}; //open, close
+    boolean clawSquished = false;
+    boolean clawSpun = false;
+
     public DcMotorEx lf;
     public DcMotorEx lb;
     public DcMotorEx rf;
     public DcMotorEx rb;
+    public DcMotorEx intakeL;
+    public DcMotorEx intakeR;
+    public Servo clawSquish;
+    public Servo clawSpin;
     private double gearCoefficient = 1;
     private double rotateCoefficient = 1;
     public static final int[][] POWER_MATRIX = { //for each of the directions
@@ -34,22 +44,41 @@ public class TestTeleOp extends OpMode {
         lb = (DcMotorEx)hardwareMap.dcMotor.get("lb");
         rf = (DcMotorEx)hardwareMap.dcMotor.get("rf");
         rb = (DcMotorEx)hardwareMap.dcMotor.get("rb");
+        intakeL = (DcMotorEx)hardwareMap.dcMotor.get("intakeL");
+        intakeR = (DcMotorEx)hardwareMap.dcMotor.get("intakeR");
+        clawSquish = hardwareMap.servo.get("clawSquish");
+        clawSpin = hardwareMap.servo.get("clawSpin");
+
         lf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         lb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        lf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-        lb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-        rf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-        rb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        intakeL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        intakeR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        lf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        lb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        intakeL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        intakeR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
         rf.setDirection(REVERSE);
         rb.setDirection(REVERSE);
+        intakeR.setDirection(REVERSE);
+
+        lf.setPower(0);
+        lb.setPower(0);
+        rf.setPower(0);
+        rb.setPower(0);
+        intakeL.setPower(0);
+        intakeR.setPower(0);
     }
 
     @Override
     public void loop() {
 
-        if (gamepad1.x) {
+        if (gamepad2.x) {
             lf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             lb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             rf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -58,10 +87,43 @@ public class TestTeleOp extends OpMode {
             lb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
             rf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
             rb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            intakeL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            intakeR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         }
 
+        if (MathUtils.equals(gamepad1.left_trigger, 1, 0.05)) {
+            intakeL.setPower(.4);
+            intakeR.setPower(.4);
+        }
 
+        if (MathUtils.equals(gamepad1.right_trigger, 1, 0.05)) {
+            intakeL.setPower(-.4);
+            intakeR.setPower(-.4);
+        }
 
+        if (gamepad1.a) { //close claw
+            if (!clawSquished) {
+                clawSquish.setPosition(squishPos[1]);
+            }
+        }
+
+        if (gamepad1.b) { //release claw
+            if (clawSquished) {
+                clawSquish.setPosition(squishPos[0]);
+            }
+        }
+
+        if (gamepad1.x) { //normal claw rotation
+            if (clawSpun) {
+                clawSpin.setPosition(spinPos[0]);
+            }
+        }
+
+        if (gamepad1.y) { //sideways claw rotation
+            if (clawSpun) {
+                clawSpin.setPosition(spinPos[1]);
+            }
+        }
 
         double transX = gamepad1.left_stick_x;
         double transY = -gamepad1.left_stick_y;
@@ -112,11 +174,11 @@ public class TestTeleOp extends OpMode {
         lb.setPower(LB * gearCoefficient);
         rf.setPower(RF * gearCoefficient);
         rb.setPower(RB * gearCoefficient);
-        telemetry.addData("THETA", translateTheta);
-        telemetry.addData("SCALE", scale);
-        telemetry.addData("LF", lf.getPower() + " " + lf.getCurrentPosition());
-        telemetry.addData("LB", lb.getPower() + " " +  lb.getCurrentPosition());
-        telemetry.addData("RF", rf.getPower() + " " + rf.getCurrentPosition());
-        telemetry.addData("RB", rb.getPower() + " " + rb.getCurrentPosition());
+//        telemetry.addData("THETA", translateTheta);
+//        telemetry.addData("SCALE", scale);
+//        telemetry.addData("LF", lf.getPower() + " " + lf.getCurrentPosition());
+//        telemetry.addData("LB", lb.getPower() + " " +  lb.getCurrentPosition());
+//        telemetry.addData("RF", rf.getPower() + " " + rf.getCurrentPosition());
+//        telemetry.addData("RB", rb.getPower() + " " + rb.getCurrentPosition());
     }
 }
