439e9927075cc33ab1cebd2501796ca4b3e7b971
unknown <aemb011@gmail.com>
2020-02-05 16:21:32 -0500

    c

--- TeamCode/src/main/java/com/millburnrobotics/skystone/Constants.java
+++ TeamCode/src/main/java/com/millburnrobotics/skystone/Constants.java
@@ -1,5 +1,6 @@
 package com.millburnrobotics.skystone;
 
+import com.millburnrobotics.lib.geometry.Pose;
 import com.qualcomm.hardware.motors.NeveRest20Gearmotor;
 import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
 
@@ -145,5 +146,42 @@ public class Constants {
         public static final double LEFT_HOOK_UP_POS = 1;
     }
 
+    public static class AutonConstants {
+        public static double X_BLUE_BLOCK_CLAW = -24 - DriveConstants.BOT_WIDTH/2.0- SideClawConstants.CLAW_EXTEND;
+        public static double Y_BLUE_BLOCK_CLAW = -24 - 4 + (DriveConstants.BOT_LENGTH/2.0- SideClawConstants.CLAW_TO_BACK);
+        public static double X_BLUE_BLOCK_INTAKE = -24+2;
+        public static double Y_BLUE_BLOCK_INTAKE = -72+8+ DriveConstants.BOT_LENGTH/2.0+4;
 
+        public static Pose BLUE_BLOCK_1 = new Pose(X_BLUE_BLOCK_CLAW, Y_BLUE_BLOCK_CLAW);
+        public static Pose BLUE_BLOCK_2 = new Pose(X_BLUE_BLOCK_CLAW, Y_BLUE_BLOCK_CLAW -8);
+        public static Pose BLUE_BLOCK_3 = new Pose(X_BLUE_BLOCK_CLAW, Y_BLUE_BLOCK_CLAW -16);
+        public static Pose BLUE_BLOCK_4 = new Pose(X_BLUE_BLOCK_CLAW, Y_BLUE_BLOCK_CLAW -24);
+        public static Pose BLUE_BLOCK_5 = new Pose(X_BLUE_BLOCK_CLAW, Y_BLUE_BLOCK_CLAW -32);
+        public static Pose BLUE_BLOCK_6 = new Pose(X_BLUE_BLOCK_INTAKE, Y_BLUE_BLOCK_INTAKE);
+
+
+        public static Pose RED_BLOCK_2 = new Pose(36,-56);
+        public static Pose RED_BLOCK_3 = new Pose(36,-48);
+        public static Pose RED_BLOCK_4 = new Pose(36,-40);
+        public static Pose RED_BLOCK_5 = new Pose(36,-32);
+        public static Pose RED_BLOCK_6 = new Pose(36,-24); // 24+4 offset -4
+
+
+        public static double X_BLUE_DELIVERY = -24- DriveConstants.BOT_WIDTH/2.0- SideClawConstants.CLAW_EXTEND;
+        public static double Y_BLUE_DELIVERY = 72-4- FieldConstants.FOUNDATION_LENGTH/2.0+(DriveConstants.BOT_LENGTH/2.0- SideClawConstants.CLAW_TO_BACK);
+
+        public static Pose BLUE_DELIVERY_1 = new Pose(X_BLUE_DELIVERY,Y_BLUE_DELIVERY +4);
+        public static Pose BLUE_DELIVERY_2 = new Pose(X_BLUE_DELIVERY,Y_BLUE_DELIVERY -6);
+        public static Pose BLUE_DELIVERY_3 = new Pose(X_BLUE_DELIVERY + SideClawConstants.CLAW_EXTEND,Y_BLUE_DELIVERY +4);
+        public static Pose BLUE_DELIVERY_4 = new Pose(X_BLUE_DELIVERY + SideClawConstants.CLAW_EXTEND,Y_BLUE_DELIVERY -6);
+
+
+        public static double X_RED_DELIVERY = -24- DriveConstants.BOT_WIDTH/2.0- SideClawConstants.CLAW_EXTEND;
+        public static double Y_RED_DELIVERY = 72-4- FieldConstants.FOUNDATION_LENGTH/2.0+(DriveConstants.BOT_LENGTH/2.0- SideClawConstants.CLAW_TO_BACK);
+
+        public static Pose RED_DELIVERY_1 = new Pose(X_RED_DELIVERY,Y_RED_DELIVERY +4);
+        public static Pose RED_DELIVERY_2 = new Pose(X_RED_DELIVERY,Y_RED_DELIVERY -6);
+        public static Pose RED_DELIVERY_3 = new Pose(X_RED_DELIVERY + SideClawConstants.CLAW_EXTEND,Y_RED_DELIVERY +4);
+        public static Pose RED_DELIVERY_4 = new Pose(X_RED_DELIVERY + SideClawConstants.CLAW_EXTEND,Y_RED_DELIVERY -6);
+    }
 }
---
+++ TeamCode/src/main/java/com/millburnrobotics/skystone/paths/DetectPath.java
@@ -0,0 +1,39 @@
+package com.millburnrobotics.skystone.paths;
+
+import com.millburnrobotics.lib.control.Path;
+import com.millburnrobotics.lib.control.PathBuilder;
+import com.millburnrobotics.lib.geometry.Pose;
+import com.millburnrobotics.lib.geometry.Waypoint;
+import com.millburnrobotics.skystone.Constants;
+import com.millburnrobotics.skystone.subsystems.Robot;
+
+import java.util.ArrayList;
+
+
+public class DetectPath implements PathContainer {
+    private int initialBlock;
+
+    public DetectPath(int initialBlock) {
+        this.initialBlock = initialBlock;
+    }
+
+    @Override
+    public Path buildPath() {
+        ArrayList<Waypoint> waypoints = new ArrayList<>();
+
+        if (Robot.getInstance().side == Constants.SIDE.BLUE) {
+            if (initialBlock == 3) {
+
+            } else {
+
+            }
+            waypoints.add(new Waypoint(Robot.getInstance().getOdometry().getPose(),0));
+            waypoints.add(new Waypoint(new Pose(-36,49,Math.PI),0));
+        } else {
+            waypoints.add(new Waypoint(Robot.getInstance().getOdometry().getPose(),Math.PI));
+            waypoints.add(new Waypoint(new Pose(36,49,0),Math.PI));
+        }
+
+        return PathBuilder.buildPath(waypoints, 1, 0.5);
+    }
+}
